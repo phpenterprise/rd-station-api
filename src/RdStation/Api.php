9c1e4bca641efeaccf9ff8d82e633218ade4a45e
Patrick Otto <patrick@phpenterprise.com>
2017-12-09 17:37:11 -0200

    Auth0 compilado

--- src/RdStation/Api.php
+++ src/RdStation/Api.php
@@ -14,10 +14,13 @@ namespace RdStation;
 
 class Api {
 
-    private $_cookie, $_token;
+    private $_cookie, $_token, $_client_id;
     protected $curl;
 
-    const RD_URI_LOGIN = 'https://app.rdstation.com.br/sign_in';
+    const RD_URI_LOGIN = 'https://app.rdstation.com.br/';
+    const RD_URI_AUTH_OPT = 'https://rdstation.auth0.com/usernamepassword/login';
+    const RD_URI_CALLBACK = 'https://rdstation.auth0.com/login/callback';
+    const RD_URI_AUTH_CALLBACK = 'https://app.rdstation.com.br/auth/auth0/callback';
     const RD_URI_LEADS = 'https://app.rdstation.com.br/leads';
     const RD_URI_DASH = 'https://app.rdstation.com.br/dashboard/load_dashboard';
     const CURL_USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13';
@@ -58,10 +61,26 @@ class Api {
 
             // call login page
             $this->curl = curl_init();
+
+            // header emulation
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
+                ':authority:app.rdstation.com.br',
+                ':method:GET',
+                ':path:/',
+                ':scheme:https',
+                'accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'accept-language:en-US,en;q=0.9,pt-BR;q=0.8,pt;q=0.7,es;q=0.6',
+                'cache-control:no-cache',
+                'pragma:no-cache',
+                'upgrade-insecure-requests:1'
+            ));
+
+            curl_setopt($this->curl, CURLOPT_USERAGENT, self::CURL_USER_AGENT);
             curl_setopt($this->curl, CURLOPT_URL, self::RD_URI_LOGIN);
             curl_setopt($this->curl, CURLOPT_COOKIEJAR, $this->_cookie);
             curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+
             $c = curl_exec($this->curl);
 
             ob_start();
@@ -78,32 +97,61 @@ class Api {
 
             // sniffing code
             foreach ($post as $script) {
+
+                $code = (stristr($script->textContent, 'Auth0Lock') && ($b = explode('Auth0Lock(\'', preg_replace('/[\s\r\n\t]+/i', '', $script->textContent))) && !empty($b[1])) ? strtok($b[1], '\'') : null;
+
+                if (!$this->_client_id && preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $code) && mb_strlen($code) === 32) {
+                    $this->_client_id = $code;
+                }
+
                 $code = (($b = explode('"', $script->textContent)) && !empty($b[1])) ? $b[1] : null;
 
-                // $value = $input->ge
                 if (!$this->_token && preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $code) && mb_strlen($code) > 40 && base64_encode(base64_decode($code, true)) === $code) {
                     $this->_token = $code;
                 }
             }
 
             // valid auth token
-            if (!$this->_token) {
+            if (!$this->_token or ! $this->_client_id) {
                 return 'Api: The authentication token is not valid, check API version or your mail and password';
             }
 
+            // reinit curl
+            curl_reset($this->curl);
+
             // refresh last connection (start login)
-            curl_setopt($this->curl, CURLOPT_URL, self::RD_URI_LOGIN);
-            curl_setopt($this->curl, CURLOPT_USERAGENT, self::CURL_USER_AGENT);
+            curl_setopt($this->curl, CURLOPT_URL, self::RD_URI_AUTH_OPT);
             curl_setopt($this->curl, CURLOPT_POST, 1);
 
+            // set auth data
+            $auth_data = array(
+                'client_id' => $this->_client_id,
+                'redirect_uri' => self::RD_URI_AUTH_CALLBACK,
+                'tenant' => 'rdstation',
+                'response_type' => 'code',
+                'connection' => 'Username-Password-Authentication',
+                'username' => $this->mail,
+                'password' => $this->pass,
+                'sso' => 'true',
+                'scope' => 'openid email'
+            );
+
             // set params
-            curl_setopt($this->curl, CURLOPT_POSTFIELDS, array(
-                'utf8' => '✓',
-                'authenticity_token' => $this->_token,
-                'user[email]' => $this->mail,
-                'user[password]' => $this->pass,
-                'user[remember_me]' => '1',
-                'commit' => 'Entrar',
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode($auth_data));
+
+            // header emulation
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
+                ':authority:rdstation.auth0.com',
+                ':method:POST',
+                ':path:/usernamepassword/login',
+                ':scheme:https',
+                'accept:*/*',
+                'content-type:application/json',
+                'cache-control:no-cache',
+                'origin:https://app.rdstation.com.br',
+                'pragma:no-cache',
+                'referer:https://app.rdstation.com.br/',
+                'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
             ));
 
             curl_setopt($this->curl, CURLOPT_COOKIEFILE, $this->_cookie);
@@ -113,6 +161,76 @@ class Api {
 
             // send data
             $a = curl_exec($this->curl);
+
+            // get status
+            $valid = stristr($a, 'wsignin1.0');
+
+            // valid authentication
+            if (!$valid) {
+                return 'Api: Authentication error, user or password is invalid or server is not avaliable';
+            }
+
+            // create DOM instance        
+            $doc = new \DOMDocument;
+            $doc->recover = true;
+            $doc->strictErrorChecking = false;
+            $doc->validateOnParse = false;
+
+            ob_start();
+            $st = @$doc->loadHTML($a, false);
+            ob_end_clean();
+
+            // valid connection
+            if (!$st) {
+                return 'Api: the auth0 callback is temporary offline, retry later';
+            }
+
+            $data_submit = array();
+
+            // read inputs
+            $post = (is_object($doc)) ? $doc->getElementsByTagName('input') : array();
+
+            // sniffing callback code (compile)
+            foreach ($post as $script) {
+                if ($script->attributes->getNamedItem('name')) {
+                    $data_submit[$script->attributes->getNamedItem('name')->value] = $script->attributes->getNamedItem('value')->value;
+                }
+            }
+
+            // reset
+            curl_reset($this->curl);
+
+            // refresh last connection (start login)
+            curl_setopt($this->curl, CURLOPT_URL, self::RD_URI_CALLBACK);
+            curl_setopt($this->curl, CURLOPT_POST, 1);
+
+            // post data
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, http_build_query($data_submit));
+
+            // header emulation
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
+                ':authority:rdstation.auth0.com',
+                ':method:POST',
+                ':path:/login/callback',
+                ':scheme:https',
+                'content-type:application/x-www-form-urlencoded',
+                'origin:https://app.rdstation.com.br',
+                'pragma:no-cache',
+                'referer:https://app.rdstation.com.br/login',
+                'upgrade-insecure-requests:1',
+                'user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+            ));
+
+            curl_setopt($this->curl, CURLOPT_COOKIEFILE, $this->_cookie);
+            curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($this->curl, CURLOPT_HEADER, true);
+
+            // send data
+            $a = curl_exec($this->curl);
+
+            // status            
             $valid = stristr($a, '/logout');
 
             // clean invalid cookie
@@ -131,6 +249,21 @@ class Api {
 
         // valid session
         if ($this->getCookieFile()) {
+
+            curl_reset($this->curl);
+
+            // header emulation
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
+                ':authority:app.rdstation.com.br',
+                ':method:GET',
+                ':path:/leads',
+                ':scheme:https',
+                'accept:*/*',
+                'cache-control:no-cache',
+                'referer:https://app.rdstation.com.br/',
+                'pragma:no-cache',
+                'upgrade-insecure-requests:1'
+            ));
 
             // refresh last connection (start login)
             curl_setopt($this->curl, CURLOPT_URL, self::RD_URI_LEADS);
